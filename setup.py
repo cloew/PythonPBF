a354fed97ed84ef62a926eb9e50bce5b9a2dc96f
Chris Loew <cloew123@gmail.com>
2014-02-02 12:19:23 -0700

    Python Package now installs helper files

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='pbf.python',
       description="Programmer's Best Friend Utility Extension for Python",
       author='', # Add your name here
       author_email='', # Add your e-mail here
-      packages=['pbf.python', 'pbf.python.Commands', 'pbf.python.templates'],
+      packages=['pbf.python', 'pbf.python.Commands', 'pbf.python.helpers', 'pbf.python.templates'],
       package_data = {'pbf.python.templates':['*']},
      )
