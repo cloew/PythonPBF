ffe1da0558ed6fe2fc8863dfc03f885c545f0551
Chris Loew <cloew123@gmail.com>
2014-02-02 11:45:01 -0700

    PBF Package Skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pbf.python',
+      version='.1',
+      description="Programmer's Best Friend Utility Extension for Python",
+      author='', # Add your name here
+      author_email='', # Add your e-mail here
+      packages=['pbf.python', 'pbf.python.Commands', 'pbf.python.templates'],
+      #package_data = {'pbf.python.templates':[]}, # Add template files
+     )
