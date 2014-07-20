b3fca55d88f3e94ae366d7dc478b9db7671b8a91
Chris Loew <cloew123@gmail.com>
2014-07-19 23:53:23 -0600

    Can now install a Python package into the setup file when creating a new Python Package

--- pbf_python/Commands/mk_pydir.py
+++ pbf_python/Commands/mk_pydir.py
@@ -2,6 +2,8 @@ from pbf.Commands import command_manager
 
 from pbf.Commands.Python.mk_pydir import MakePyDir as CoreMakePyDir
 
+from pbf_python.Commands.insert_setup_package import InsertSetupPackage
+
 class MakePyDir:
     """ Represents a command that makes a python directory """
     category = "mk"
@@ -11,16 +13,22 @@ class MakePyDir:
     def addArguments(self, parser):
         """ Add arguments to the parser """
         parser.add_argument('directory', action='store', help='Directory to create as a Python package')
+        parser.add_argument('-i', '--install', action='store_true', help='Add the new package to the setup install file')
     
     def run(self, arguments):
         """ Run the command """
         dirname = arguments.directory
+        install = arguments.install
         print "Creating Python Directory:", dirname
-        self.makePyDir(dirname)
+        self.makePyDir(dirname, install=install)
         
-    def makePyDir(self, dirname):
+    def makePyDir(self, dirname, install=False):
         """ Create the Python directory """
         CoreMakePyDir().makePyDir(dirname)
+        
+        if install:
+            insertCommand = InsertSetupPackage()
+            insertCommand.insertPackage(dirname)
     
     def help(self):
         """ Print the usage of the Make Py Dir """
