c26835a939cf1bc6f10314fe82686c3494c5c128
Chris Loew <cloew123@gmail.com>
2015-02-19 00:02:09 -0700

    Fixed bad class reference to the MakePyDir Command

--- pbf_python/command_map.py
+++ pbf_python/command_map.py
@@ -1,7 +1,7 @@
 from pbf.Commands.command_manager import CommandConfig, RegisterCommands
 
 commands = [CommandConfig("insert setup-package", "pbf_python.Commands.insert_setup_package.InsertSetupPackage", description="Inserts a package into the setup package"),
-            CommandConfig("mk pydir", "pbf_python.Commands.mk_pydir.MkPydir", description="Makes a Python Directory"),
+            CommandConfig("mk pydir", "pbf_python.Commands.mk_pydir.MakePyDir", description="Makes a Python Directory"),
             CommandConfig("new class", "pbf_python.Commands.new_class.NewClass", description="Create a new Python Class"),
             CommandConfig("new main", "pbf_python.Commands.new_main.NewMain", description="Create a new Python main file")]
 
